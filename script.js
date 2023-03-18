60807040aab0ef6b3f8e829f348a0d91a56f90d1
dragonfireshield <dragonfireshield@pm.me>
2023-03-18 19:45:39 +0100

    implemented game interface

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 const hands = ['rock', 'paper', 'scissors'];
+const heading = document.createElement('h1');
+const rockButton = document.createElement('button');
+const paperButton = document.createElement('button');
+const scissorsButton = document.createElement('button');
+const resultComputer = document.createElement('p');
+const resultPlayer = document.createElement('p');
+const resultComputerScore = document.createElement('p');
+const resultPlayerScore = document.createElement('p');
+
+heading.textContent = 'Rock, Paper, Scissors!';
+rockButton.textContent = 'Rock';
+paperButton.textContent = 'Paper';
+scissorsButton.textContent = 'Scissors';
+resultComputer.textContent = '?';
+resultPlayer.textContent = '?';
+resultComputerScore.textContent = '0';
+resultPlayerScore.textContent = '0';
+
+document.body.appendChild(heading);
+document.body.appendChild(rockButton);
+document.body.appendChild(paperButton);
+document.body.appendChild(scissorsButton);  
+document.body.appendChild(resultComputer);  
+document.body.appendChild(resultPlayer);  
+document.body.appendChild(resultComputerScore);  
+document.body.appendChild(resultPlayerScore);  
+
+rockButton.addEventListener('click', (e) => playRound(getComputerChoice(), e.target.textContent.toLowerCase()))
+paperButton.addEventListener('click', (e) => playRound(getComputerChoice(), e.target.textContent.toLowerCase()))
+scissorsButton.addEventListener('click', (e) => playRound(getComputerChoice(), e.target.textContent.toLowerCase()))
 
 function getComputerChoice() {
     const random = Number(Math.floor(Math.random() * 3));
@@ -6,33 +36,45 @@ function getComputerChoice() {
     return choice;
 }
 
-function getInput() {
-    let input = '';
-    do {
-        input = prompt("Rock, paper, scissors? ");    
-    } while (!hands.includes(input.toLowerCase()));
-    return input;
-}
+// function getInput() {
+//     let input = '';
+//     do {
+//         input = prompt("Rock, paper, scissors? ");    
+//     } while (!hands.includes(input.toLowerCase()));
+//     return input;
+// }
 
 function playRound(computerSelection, playerSelection) {
+    resultComputer.textContent = computerSelection;
+    resultPlayer.textContent = playerSelection;
     if (computerSelection === 'rock' && playerSelection === 'paper') {
         playerScore++;
         console.log(`computerSelection: ${computerSelection}`, `playerSelection: ${playerSelection}`);
+        resultComputerScore.textContent = computerScore;
+        resultPlayerScore.textContent = playerScore;
         return "You win! Paper beats rock.";
     } else if (computerSelection === 'paper' && playerSelection === 'scissors'){
         playerScore++;
         console.log(`computerSelection: ${computerSelection}`, `playerSelection: ${playerSelection}`);
+        resultComputerScore.textContent = computerScore;
+        resultPlayerScore.textContent = playerScore;
         return "You win! Scissors beats paper.";
     } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
         playerScore++;
         console.log(`computerSelection: ${computerSelection}`, `playerSelection: ${playerSelection}`);
+        resultComputerScore.textContent = computerScore;
+        resultPlayerScore.textContent = playerScore;
         return "You win! Rock beats scissors.";
     } else if (computerSelection !== playerSelection) {
         computerScore++;
         console.log(`computerSelection: ${computerSelection}`, `playerSelection: ${playerSelection}`);
+        resultComputerScore.textContent = computerScore;
+        resultPlayerScore.textContent = playerScore;
         return `You lose!`;
     } else {
         console.log(`computerSelection: ${computerSelection}`, `playerSelection: ${playerSelection}`);
+        resultComputerScore.textContent = computerScore;
+        resultPlayerScore.textContent = playerScore;
         return `It's a draw! You: ${playerSelection}, Computer: ${computerSelection}`;
     }
 }
@@ -40,23 +82,23 @@ function playRound(computerSelection, playerSelection) {
 let playerScore = 0;
 let computerScore = 0;
 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
+// function game() {
+//     playerScore = 0;
+//     computerScore = 0;
 
-    for (let index = 0; index < 5; index++) {
-        const computerSelection = getComputerChoice();
-        const playerSelection = getInput();
-        console.log(playRound(computerSelection, playerSelection), `playerScore: ${playerScore}`, `computerScore: ${computerScore}`);
-    }
+//     for (let index = 0; index < 5; index++) {
+//         const computerSelection = getComputerChoice();
+//         const playerSelection = getInput();
+//         console.log(playRound(computerSelection, playerSelection), `playerScore: ${playerScore}`, `computerScore: ${computerScore}`);
+//     }
 
-    if (playerScore === computerScore) {
-        console.log("It's a draw!!");
-    } else {
-        const winner = playerScore > computerScore ? 'Player' : 'Computer';
-        const winnerScore = playerScore > computerScore ? playerScore : computerScore;
-        console.log(`${winner} wins with a score of ${winnerScore}`);
-    }
-}
+//     if (playerScore === computerScore) {
+//         console.log("It's a draw!!");
+//     } else {
+//         const winner = playerScore > computerScore ? 'Player' : 'Computer';
+//         const winnerScore = playerScore > computerScore ? playerScore : computerScore;
+//         console.log(`${winner} wins with a score of ${winnerScore}`);
+//     }
+// }
 
-console.log(game());
+// console.log(game());
